b6e8b76ff5de3b48917b6234d86116fef56aa234
Anonymous <bsftraders4@gmail.com>
2025-10-02 13:43:10 +0500

    Routers added

--- routers.py
+++ routers.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 
 from auth.views import auth_router
+from api.views.category import category_router
+from api.views.content_version import content_version_router
+from api.views.content import content_router
+from api.views.example import example_router
+from api.views.lesson import lesson_router
+from api.views.quiz import quiz_router
 
 app = FastAPI()
+
+app.include_router(category_router)
+app.include_router(content_version_router)
+app.include_router(content_router)
+app.include_router(example_router)
+app.include_router(lesson_router)
+app.include_router(quiz_router)
 
 app.include_router(auth_router)
